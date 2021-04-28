2ecc811e9a0546e4e1aa0c77f5dfa192d3185d2e
fareed069417 <shoeb.ahmad@v-empower.com>
2021-04-28 21:05:24 +0530

    First commit

---
+++ native/README.md
@@ -0,0 +1,19 @@
+# Jitsi Meet SDK samples
+
+This repository contains sample applications for Android and iOS which use the [Jitsi Meet] SDK. They are organized
+as follows:
+
+## Android
+
+* [android/java](android/java): Android example, written in Java
+* [android/kotlin](android/kotlin): Android example, written in Kotlin
+
+## iOS
+
+* [ios/objc](ios/objc): iOS examples, written in Objective-C
+* [ios/swift](ios/swift): iOS example, written in Swift
+* [ios/swift-pip](ios/swift-pip): iOS example, written in Swift and showing how Picture-in-Picture can be implemented
+
+NOTE: You must be using Xcode >= 12.2 for the iOS examples to run.
+
+[Jitsi Meet]: https://github.com/jitsi/jitsi-meet
---
+++ native/ios/swift/JitsiSDKTest/src/ViewController.swift
@@ -0,0 +1,85 @@
+/*
+ * Copyright @ 2017-present 8x8, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import UIKit
+import JitsiMeetSDK
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var videoButton: UIButton?
+    @IBOutlet weak var roomName: UITextField!
+    
+    fileprivate var jitsiMeetView: JitsiMeetView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let defaultOptions = JitsiMeetConferenceOptions.fromBuilder { (builder) in
+            // for JaaS replace url with https://8x8.vc
+            builder.serverURL = URL(string: "https://meet.jit.si")
+            // for JaaS use the obtained Jitsi JWT
+            // builder.token = "SampleJWT"
+            builder.welcomePageEnabled = false
+            // Set different feature flags
+            builder.setFeatureFlag("toolbox.enabled", withBoolean: false)
+            builder.setFeatureFlag("filmstrip.enabled", withBoolean: false)
+        }
+        
+        JitsiMeet.sharedInstance().defaultConferenceOptions = defaultOptions
+    }
+    
+    @IBAction func openJitsiMeet(sender: Any?) {
+        let room: String = roomName.text!
+        if(room.count < 1) {
+            return
+        }
+        
+        // create and configure jitsimeet view
+        let jitsiMeetView = JitsiMeetView()
+        jitsiMeetView.delegate = self
+        self.jitsiMeetView = jitsiMeetView
+        let options = JitsiMeetConferenceOptions.fromBuilder { (builder) in
+            // for JaaS use <tenant>/<roomName> format
+            builder.room = room
+            // Settings for audio and video
+            // builder.audioMuted = true;
+            // builder.videoMuted = true;
+        }
+                
+        // setup view controller
+        let vc = UIViewController()
+        vc.modalPresentationStyle = .fullScreen
+        vc.view = jitsiMeetView
+        
+        // join room and display jitsi-call
+        jitsiMeetView.join(options)
+        present(vc, animated: true, completion: nil)
+        
+    }
+    
+    fileprivate func cleanUp() {
+        if(jitsiMeetView != nil) {
+            dismiss(animated: true, completion: nil)
+            jitsiMeetView = nil
+        }
+    }
+}
+
+extension ViewController: JitsiMeetViewDelegate {
+    func conferenceTerminated(_ data: [AnyHashable : Any]!) {
+        cleanUp()
+    }
+}
