2726b164ec46efa15600763a149cd42fdd887980
chepury-bharathraj <bharatraj@v-empower.com>
2021-06-28 17:36:38 +0530

    Updated With ScreenShare Code

--- reactnative/MeetHourSDKTest/App.js
+++ reactnative/MeetHourSDKTest/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react';
 import {
   Text,
   TextInput,
@@ -7,16 +7,16 @@ import {
   Switch,
   ImageBackground,
   Image,
-  ScrollView
+  ScrollView,
+  Platform,
 } from 'react-native';
 
-import MeetHour, { MeetHourView } from 'react-native-meet-hour-sdk';
+import MeetHour, {MeetHourView} from 'react-native-meet-hour-sdk';
 
 import styles from './styles/styles';
 import strings from './lang/strings';
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.onConferenceTerminated = this.onConferenceTerminated.bind(this);
@@ -30,13 +30,12 @@ class App extends Component {
       displayName: '',
       email: '',
       audioMuted: false,
-      videoMuted: false
-    }
+      videoMuted: false,
+    };
   }
 
   runMeet() {
     this.setState({showMeet: true});
-    
     const meetingInfo = {
       serverUrl: this.state.serverUrl,
       roomName: this.state.roomName,
@@ -50,38 +49,27 @@ class App extends Component {
       videoMuted: this.state.videoMuted,
     };
 
-    // Parameters for iOS devices
-    MeetHour.roomName = meetingInfo.roomName;
-    MeetHour.subject = meetingInfo.subject;
-    MeetHour.audioMuted = meetingInfo.audioMuted;
-    MeetHour.videoMuted = meetingInfo.videoMuted;
-
     setTimeout(() => {
-      (Platform.OS === 'ios') ?
-        MeetHour.call(
-          meetingInfo.serverUrl + '/' + meetingInfo.roomName,
-          meetingInfo.userInfo,
-        ) :
-        MeetHour.call(meetingInfo);
+      MeetHour.join_meethour(meetingInfo);
     }, 1000);
-  
+
     this.cleanUp();
   }
 
   onConferenceJoined(nativeEvent) {
     /* Conference joined event */
-    console.log(nativeEvent)
+    console.log(nativeEvent);
   }
 
   onConferenceWillJoin(nativeEvent) {
     /* Conference will join event */
-    console.log(nativeEvent)
+    console.log(nativeEvent);
   }
 
   onConferenceTerminated(nativeEvent) {
     /* Conference terminated event */
-    console.log(nativeEvent)
-    this.setState({ showMeet: false });
+    console.log(nativeEvent);
+    this.setState({showMeet: false});
   }
 
   cleanUp() {
@@ -92,11 +80,13 @@ class App extends Component {
       displayName: '',
       email: '',
       audioMuted: false,
-      videoMuted: false
-    })
+      videoMuted: false,
+    });
   }
 
   render() {
+    console.log('inside render ', this.state.showMeet);
+
     return (
       <View style={styles.container}>
         {this.state.showMeet ? (
@@ -104,77 +94,96 @@ class App extends Component {
             onConferenceTerminated={this.onConferenceTerminated}
             onConferenceJoined={this.onConferenceJoined}
             onConferenceWillJoin={this.onConferenceWillJoin}
+            // eslint-disable-next-line react-native/no-inline-styles
             style={{
               flex: 1,
               height: '100%',
               width: '100%',
             }}
           />
-        ) :
-        
-        (<ScrollView>
-          <ImageBackground
-            source={require('./images/MeetHour_logo.png')}
-            style={styles.image}
-            resizeMode='contain'
-          />
-          <TextInput
-            style={styles.textInput}
-            value={this.state.serverUrl}
-            placeholder={strings.placeholders.serverURL}
-            onChangeText={text => this.setState({serverUrl: text})}
-            keyboardType={'url'}
-            autoCapitalize={'none'}
-          />
-          <TextInput
-            style={styles.textInput}
-            value={this.state.roomName}
-            placeholder={strings.placeholders.roomname}
-            onChangeText={text => this.setState({roomName: text})}
-          />
-          <TextInput
-            style={styles.textInput}
-            value={this.state.subject}
-            placeholder={strings.placeholders.subject}
-            onChangeText={text => this.setState({subject: text})}
-          />
-          <TextInput
-            style={styles.textInput}
-            value={this.state.displayName}
-            placeholder={strings.placeholders.displayName}
-            onChangeText={text => this.setState({displayName: text})}
-            autoCapitalize={'words'}
-          />
-          <TextInput
-            style={styles.textInput}
-            value={this.state.email}
-            placeholder={strings.placeholders.email}
-            onChangeText={text => this.setState({email: text})}
-            keyboardType={'email-address'}
-          />
-          <View style={styles.switch}>
-            <Text>{strings.text.startWithAudioMuted}</Text>
-            <Switch onValueChange={() => this.setState({audioMuted: !this.state.audioMuted})} value={this.state.audioMuted} />
-          </View>
-          <View style={styles.switch}>
-            <Text>{strings.text.startWithVideoMuted}</Text>
-            <Switch onValueChange={() => this.setState({videoMuted: !this.state.videoMuted})} value={this.state.videoMuted} />
-          </View>
-          <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around'}}>
-            <Text>{strings.avatar.avatar}</Text>
-            <Image
-              style={styles.avatar}
-              resizeMode='contain'
-              source={{uri:strings.avatar.avatarURL}}
+        ) : (
+          // eslint-disable-next-line react-native/no-inline-styles
+          <ScrollView style={{marginTop: '10%'}}>
+            <ImageBackground
+              source={require('./images/MeetHour_logo.png')}
+              style={styles.image}
+              resizeMode="contain"
             />
-          </View>
-          <TouchableOpacity onPress={() => this.runMeet()} style={styles.button}>
-            <Text style={styles.buttonText}>{strings.buttons.join}</Text>
-          </TouchableOpacity>
-        </ScrollView>
+            <TextInput
+              style={styles.textInput}
+              value={this.state.serverUrl}
+              placeholder={strings.placeholders.serverURL}
+              onChangeText={(text) => this.setState({serverUrl: text})}
+              keyboardType={'url'}
+              autoCapitalize={'none'}
+            />
+            <TextInput
+              style={styles.textInput}
+              value={this.state.roomName}
+              placeholder={strings.placeholders.roomname}
+              onChangeText={(text) => this.setState({roomName: text})}
+            />
+            <TextInput
+              style={styles.textInput}
+              value={this.state.subject}
+              placeholder={strings.placeholders.subject}
+              onChangeText={(text) => this.setState({subject: text})}
+            />
+            <TextInput
+              style={styles.textInput}
+              value={this.state.displayName}
+              placeholder={strings.placeholders.displayName}
+              onChangeText={(text) => this.setState({displayName: text})}
+              autoCapitalize={'words'}
+            />
+            <TextInput
+              style={styles.textInput}
+              value={this.state.email}
+              placeholder={strings.placeholders.email}
+              onChangeText={(text) => this.setState({email: text})}
+              keyboardType={'email-address'}
+            />
+            <View style={styles.switch}>
+              <Text>{strings.text.startWithAudioMuted}</Text>
+              <Switch
+                onValueChange={() =>
+                  this.setState({audioMuted: !this.state.audioMuted})
+                }
+                value={this.state.audioMuted}
+              />
+            </View>
+            <View style={styles.switch}>
+              <Text>{strings.text.startWithVideoMuted}</Text>
+              <Switch
+                onValueChange={() =>
+                  this.setState({videoMuted: !this.state.videoMuted})
+                }
+                value={this.state.videoMuted}
+              />
+            </View>
+            <View
+              // eslint-disable-next-line react-native/no-inline-styles
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-around',
+              }}>
+              <Text>{strings.avatar.avatar}</Text>
+              <Image
+                style={styles.avatar}
+                resizeMode="contain"
+                source={{uri: strings.avatar.avatarURL}}
+              />
+            </View>
+            <TouchableOpacity
+              onPress={() => this.runMeet()}
+              style={styles.button}>
+              <Text style={styles.buttonText}>{strings.buttons.join}</Text>
+            </TouchableOpacity>
+          </ScrollView>
         )}
       </View>
-    )
+    );
   }
 }
 
