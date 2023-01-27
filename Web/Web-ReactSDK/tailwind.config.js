dde05f218d1a04809064b8baac91a766140c0384
Restyled.io <commits@restyled.io>
2023-01-27 17:03:33 +0000

    Restyled by prettier

--- Web/Web-ReactSDK/tailwind.config.js
+++ Web/Web-ReactSDK/tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content :
-          [
-            "./src/**/*.{html,js,ts,jsx,tsx}",
-          ],
-  theme : {
-    extend : {},
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
   },
-  plugins : [],
-}
+  plugins: [],
+};
