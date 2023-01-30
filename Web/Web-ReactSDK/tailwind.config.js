2de129bcab800ee157ebddbfa834421539355610
Restyled.io <commits@restyled.io>
2023-01-30 17:35:14 +0000

    Restyled by clang-format

--- Web/Web-ReactSDK/tailwind.config.js
+++ Web/Web-ReactSDK/tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
+  content : [ "./src/**/*.{html,js,ts,jsx,tsx}" ],
+  theme : {
+    extend : {},
   },
-  plugins: [],
+  plugins : [],
 };
