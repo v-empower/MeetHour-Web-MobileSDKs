91c314ba00c424cc49269a5b9ba3aef8034f273b
iamzeeshanali <xeeshanali786@gmail.com>
2023-02-20 21:19:22 +0530

    Removed credentials from all files

--- Mobile/ReactNative/MeetHourSDKTest/src/constants/index.js
+++ Mobile/ReactNative/MeetHourSDKTest/src/constants/index.js
@@ -1,9 +1,9 @@
 // Developer Credentials - Signup for Meet Hour Developer Plan (https://meethour.io)
-export const CLIENT_ID = '890py815uk6xpy2qz2ozup60y9xw15q530tx';
-export const CLIENT_SECRET = 'a7b7ca34db7d7c54d76cb1dbe94f41a353e425d7a660865e7cf86a5d824ae033';
-export const API_KEY = '653ca41a235215ed4531959c9b4b217b91c41324e1949aae9f09f5c4d505fcb6';
-export const EMAIL = 'zeeshan.ali@v-empower.com';
-export const PASSWORD = 'u98Iu9wA';
+export const CLIENT_ID = '';
+export const CLIENT_SECRET = '';
+export const API_KEY = '';
+export const EMAIL = '';
+export const PASSWORD = '';
 
 // If you don't have password, you can generate from here. https://portal.meethour.io/customer/changepassword
 // You can also go to forgot password page and set a new password. https://portal.meethour.io/customer/forgotpassword
