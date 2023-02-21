2a48fbc35515dbbe00b685f7dc4a7885f9b56195
fareed069417 <shoeb.ahmad@v-empower.com>
2023-02-21 17:40:59 +0530

    eslint update & exclude async storage.

--- Mobile/ReactNative/MeetHourSDKTest/.eslintrc.js
+++ Mobile/ReactNative/MeetHourSDKTest/.eslintrc.js
@@ -1,10 +1,4 @@
 module.exports = {
   root: true,
-  parser: "eslint/parser",
-  plugins: ["eslint"],
-  extends: [
-    "eslint:recommended",
-    "plugin:eslint/eslint-recommended",
-    "plugin:eslint/recommended",
-  ],
+  extends: '@react-native-community'
 };
