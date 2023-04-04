37ce74fef5d386f0b8cf8cf93b8d4021e108ad78
Restyled.io <commits@restyled.io>
2023-04-04 10:21:01 +0000

    Restyled by whitespace

--- Web/PHP/CorePHP/meethour-php-example.sql
+++ Web/PHP/CorePHP/meethour-php-example.sql
@@ -37,4 +37,3 @@ SET time_zone = "+00:00";
 --
 CREATE TABLE IF NOT EXISTS `credentials` (
     `access_token` varchar(5000) NOT NULL) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
-
