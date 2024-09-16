5ee24b16e923708dda9cec2b0c319431cb506743
Restyled.io <commits@restyled.io>
2024-09-16 14:19:37 +0000

    Restyled by astyle

--- Web/DotNet(C#)/Asp.Net/MeethourExample-Asp/Models/ErrorViewModel.cs
+++ Web/DotNet(C#)/Asp.Net/MeethourExample-Asp/Models/ErrorViewModel.cs
@@ -1,7 +1,16 @@
 namespace MeethourExample_Asp.Models {
 public class ErrorViewModel {
-  public bool Error { get; set; }
-  public string? Message { get; set; }
-  public string? AccessToken { get; set; }
+    public bool Error {
+        get;
+        set;
+    }
+    public string? Message {
+        get;
+        set;
+    }
+    public string? AccessToken {
+        get;
+        set;
+    }
 }
 }
