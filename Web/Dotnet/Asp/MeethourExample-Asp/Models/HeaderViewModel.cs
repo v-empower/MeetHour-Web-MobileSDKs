9ad7d484d974cf787b6f6ca48684bcb50b8c21bf
Restyled.io <commits@restyled.io>
2024-09-16 14:17:24 +0000

    Restyled by astyle

--- Web/Dotnet/Asp/MeethourExample-Asp/Models/HeaderViewModel.cs
+++ Web/Dotnet/Asp/MeethourExample-Asp/Models/HeaderViewModel.cs
@@ -1,9 +1,21 @@
 ﻿namespace MeethourExample_Asp.Models {
 public class HeaderViewModel {
-  public string? LogoImageUrl { get; set; }
-  public string? HomeUrl { get; set; }
-  public string? ScheduleMeetingUrl { get; set; }
-  public string? JoinMeetingUrl { get; set; }
+    public string? LogoImageUrl {
+        get;
+        set;
+    }
+    public string? HomeUrl {
+        get;
+        set;
+    }
+    public string? ScheduleMeetingUrl {
+        get;
+        set;
+    }
+    public string? JoinMeetingUrl {
+        get;
+        set;
+    }
 }
 
 }
