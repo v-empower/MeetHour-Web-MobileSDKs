2197a5486e586dbbc8cc25b685eb0c356da19cc0
fareed069417 <shoeb.ahmad@v-empower.com>
2025-07-02 22:11:52 +0530

    Flutter Example Update with Java and Kotlin version

---
+++ Mobile/Flutter/meethoursdkexample/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_native_timezone
+import shared_preferences_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterNativeTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterNativeTimezonePlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+}
