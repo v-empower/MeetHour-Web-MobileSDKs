79cee7c9f412171a2b366aad8b80e1be0fab2a84
fareed069417 <shoeb.ahmad@v-empower.com>
2025-08-06 13:21:47 +0530

    Removing other version of Flutter

--- Mobile/Flutter/meethoursdkexample/lib/constants/index.dart
+++
@@ -1,5 +0,0 @@
-const String CLIENT_ID = "";
-const String CLIENT_SECRET = "";
-const String API_KEY = "";
-const String USERNAME = "";
-const String PASSWORD = "";
--- Mobile/Flutter/meethoursdkexample/lib/main.dart
+++
@@ -1,5 +0,0 @@
-import 'package:meethoursdkexample/pages/homepage.dart';
-import 'package:flutter/material.dart';
-
-void main() => runApp(Homepage());
-
